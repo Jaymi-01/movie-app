62ba477d9f42fabfdd3b5c81bccf3fdb5afc1a2e
Jaymi-01 <millerjoel7597@gmail.com>
2025-03-04 23:44:05 +0100

    fix image display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,13 +10,19 @@ const Card = () => {
                 <span className="text-lg">Genres:</span>
                 <span className="font-semibold text-primary">Crime Drama</span>
             </div>
-            <span>Original Language: <span>EN</span></span>
-            <span>Release Date: <span>1972-03-04</span></span>
-            <p><span>Summary:</span>
-                <span>Summary text</span>
+            <span className="flex gap-x-2">Original Language: 
+                <span className="mr-2 uppercase">EN</span>
+            </span>
+            <span className="flex gap-x-2">Release Date: 
+                <span className="mr-2 text-secondary">1972-03-04</span>
+            </span>
+            <p className="flex flex-col gap-y-1">
+                <span className="text-primary">Summary:</span>
+                <span className="first-letter:pl-2">Summary text</span>
             </p>
         </div>
-        <img className="h-[600px]" src="/godfather.jpg" alt="Movie Image" />
+        <img className="object-cover rounded-xl opacity-50 group-hover:opacity-100 
+        transition-opacity duration-500" src="/godfather.jpg" alt="Movie Image" />
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
 import Card from "./Card"
 const Home = () => {
+    const [cardwidth, setCardWidth] = useState(500)
+    const cardsInRow = 5
+    const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardsInRow)
   return (
     <div className="flex justify-center items-center">
         <div className="flex flex-wrap">
