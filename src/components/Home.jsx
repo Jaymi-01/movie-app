956e233158fa533b4fa275e5aab7bbda58797c6a
Jaymi-01 <millerjoel7597@gmail.com>
2025-03-05 21:44:13 +0100

    Api working in console

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Card from "./Card"
 
 const Home = () => {
     const [cardWidth, setCardWidth] = useState(500)
     const cardsInRow = 5
     const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardsInRow)
+    const [movies, setMovies] =useState([])
+
+    useEffect(() => {
+        const getMovies = async () => {
+            const url = 'https://tvshow.p.rapidapi.com/Serie/Popular?Page=1&Language=en-US';
+            const options = {
+                method: 'GET',
+                headers: {
+                    'x-rapidapi-key': 'eba2e58da8msh782801184884a31p1f01dfjsn65aa7ffae0a0',
+                    'x-rapidapi-host': 'tvshow.p.rapidapi.com'
+                }
+            };
+
+            try {
+                const response = await fetch(url, options);
+                const result = await response.json();
+                console.log(result);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        getMovies()
+    }, [])
+    
   return (
     <div className="flex justify-center items-center" style={{width: wrapperWidth}}>
         <div className="flex flex-wrap">
