57e4bd2120fee29afc7a078636399ae91f9990c6
Jaymi-01 <millerjoel7597@gmail.com>
2025-03-05 23:43:21 +0100

    added navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,16 @@
+import { IoMdArrowDropleftCircle,  IoMdArrowDroprightCircle} from "react-icons/io";
 const Navigation = () => {
   return (
-    <div>
-        <div>
-            <span>Left arrow</span>
-            <p>1</p>
-            <span>Right arrow</span>
+    <div className="relative z-10">
+        <div className="fixed bottom-5 left-5 flex items-center gap-x-2 text-2xl 
+        bg-yellow-500/50 rounded-full px-2">
+            <span className="cursor-pointer"><IoMdArrowDropleftCircle /></span>
+            <p className="text-xl select-none">1</p>
+            <span><IoMdArrowDroprightCircle /></span>
         </div>
-        <select>
+        <select className="top-5 left-5 bg-gray-200/90 text-sm tracking-wide 
+        text-gray-700 uppercase rounded-md outline-none p-1 cursor-pointer 
+        hover:bg-gray-200">
             <option value="TopRated">Top Rated</option>
             <option value="Popular">Popular</option>
             <option value="Upcoming">Upcoming</option>
