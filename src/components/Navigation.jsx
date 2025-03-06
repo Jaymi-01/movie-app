c04e1dd0c993e4175d132315c70f0b0d30ef0d46
Jaymi-01 <millerjoel7597@gmail.com>
2025-03-06 23:29:03 +0100

    error fixed, now displaying the correct value

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,16 @@
 import { IoMdArrowDropleftCircle,  IoMdArrowDroprightCircle} from "react-icons/io";
-const Navigation = (page, setPage) => {
+const Navigation = ({page, setPage, setGroup}) => {
   return (
     <div className="relative z-10">
         <div className="fixed bottom-5 left-5 flex items-center gap-x-2 text-2xl 
         bg-yellow-500/50 rounded-full px-2">
-            <span className="cursor-pointer"><IoMdArrowDropleftCircle /></span>
-            <p className="text-xl select-none">1</p>
-            <span><IoMdArrowDroprightCircle /></span>
+            <span onClick={() => page != 1 && setPage((page) => page - 1)} className="cursor-pointer">
+              <IoMdArrowDropleftCircle />
+            </span>
+            <p className="text-xl select-none">{page}</p>
+            <span onClick={() => setPage((page) => page + 1)} className="cursor-pointer">
+              <IoMdArrowDroprightCircle />
+            </span>
         </div>
         <select className=" fixed top-5 left-5 bg-gray-200/90 text-sm tracking-wide 
         text-gray-700 uppercase rounded-md outline-none p-1 cursor-pointer 
